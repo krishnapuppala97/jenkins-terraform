0eb5905bc2f3c788b8c7f72bcade2a59ca2efcb8
krishnapuppala97 <95561581+krishnapuppala97@users.noreply.github.com>
2023-01-05 12:04:43 +0530

    Update myvpc.tf

--- myvpc.tf
+++ myvpc.tf
@@ -55,7 +55,7 @@ resource "aws_route_table" "myrt" {
     Name = "myrt"
   }
 }
-rvbbbesource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.mypubsub.id
   route_table_id = aws_route_table.myrt.id
 }
